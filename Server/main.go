50af4c65cbabdceabb78c3070a7bc28b8db7b432
Luis Pombal <luis.pombal@cimcorp.com>
2020-01-11 20:49:07 +0200

    Hardcoded new JSON in the api POST

--- Server/main.go
+++ Server/main.go
@@ -36,13 +36,22 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func createEvent(w http.ResponseWriter, r *http.Request) {
+	newEvent := event {
+		ID:          "3",
+		Title:       "Third to Golang",
+		Description: "333",
+    }
+	
+	/*
 	var newEvent event
+	
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
 	}
 
 	json.Unmarshal(reqBody, &newEvent)
+	*/
 	events = append(events, newEvent)
 	w.WriteHeader(http.StatusCreated)
 
